9a2d5578b63c3daec5147568b2b1dc729b1b5aeb
NikitaPeshko <79310641+NikitaPeshko@users.noreply.github.com>
2021-03-13 13:24:02 +0300

    Lab1
    4 encryption methods are implemented

---
+++ lab 1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.DecrText = new System.Windows.Forms.TextBox();
+            this.EncrText = new System.Windows.Forms.TextBox();
+            this.KeyNumb = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.KeyNumb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(241, 362);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(114, 51);
+            this.EncryptButton.TabIndex = 0;
+            this.EncryptButton.Text = "Зашифровать";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(427, 362);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(114, 51);
+            this.DecryptButton.TabIndex = 3;
+            this.DecryptButton.Text = "Расшифровать";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // DecrText
+            // 
+            this.DecrText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DecrText.Location = new System.Drawing.Point(42, 24);
+            this.DecrText.Multiline = true;
+            this.DecrText.Name = "DecrText";
+            this.DecrText.Size = new System.Drawing.Size(313, 286);
+            this.DecrText.TabIndex = 6;
+            // 
+            // EncrText
+            // 
+            this.EncrText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EncrText.Location = new System.Drawing.Point(427, 24);
+            this.EncrText.Multiline = true;
+            this.EncrText.Name = "EncrText";
+            this.EncrText.Size = new System.Drawing.Size(330, 286);
+            this.EncrText.TabIndex = 7;
+            this.EncrText.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // KeyNumb
+            // 
+            this.KeyNumb.Location = new System.Drawing.Point(652, 408);
+            this.KeyNumb.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.KeyNumb.Name = "KeyNumb";
+            this.KeyNumb.Size = new System.Drawing.Size(120, 20);
+            this.KeyNumb.TabIndex = 8;
+            this.KeyNumb.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(603, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ключ";
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(652, 407);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new System.Drawing.Size(120, 20);
+            this.KeyBox.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Жд изгородь",
+            "Столбцовый метод",
+            "Виженера",
+            "Поворачивающаяся решетка"});
+            this.comboBox1.Location = new System.Drawing.Point(652, 362);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(603, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Метод";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.KeyBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KeyNumb);
+            this.Controls.Add(this.EncrText);
+            this.Controls.Add(this.DecrText);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Name = "Form1";
+            this.Text = "Shifrator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.KeyNumb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.TextBox DecrText;
+        private System.Windows.Forms.TextBox EncrText;
+        private System.Windows.Forms.NumericUpDown KeyNumb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox KeyBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
